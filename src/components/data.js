8bba547eccbe934a41193061d46b7a878d3ca879
Adrian Kim D. Requillo <Akrequillo@gmail.com>
2023-03-26 03:05:35 +0800

    first commit

---
+++ src/components/data.js
@@ -0,0 +1,26 @@
+const menu = [
+    {
+        id : 1,
+        name: "Grilled Balut",
+        price: 35,
+        category: "Grilled",
+        image: "/grilled.jpg"
+    },
+    {
+        id : 2,
+        name: "Sizzling Sisig",
+        price: 100,
+        category: "Pork",
+        image: "/sisig.jpg"
+
+    },
+    {
+
+        id : 3,
+        name: "Braised Turkey",
+        price: 250,
+        category: "Grilled",
+        image: "/turkey.jpg"
+    }
+]
+export default menu
